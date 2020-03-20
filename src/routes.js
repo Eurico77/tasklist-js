217c9c5b1d604084e8ca6c892ff0c6b6299e05e1
Eurico77 <euricomagalhaes61@gmail.com>
2020-03-20 19:54:08 -0300

    add yarn, and prettier  and eslint

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/test', ( req , res )=>{
-
-    return res.json({ok: true})
-
-})
-
+routes.get('/teste', (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
