dfd69a94fd3d0e9f988239f0a7e192424209401e
Eurico77 <euricomagalhaes61@gmail.com>
2020-03-20 20:47:39 -0300

    add migrations and sequelizere

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'admin',
+  password: 'postgres',
   database: 'tasklist',
   define: {
     timestamps: true,
